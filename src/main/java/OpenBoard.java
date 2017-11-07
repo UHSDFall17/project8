33fc74b4e37080ee79dcd8532ec8414ceff5e3d5
nvattipalli <nvattipalli@uh.edu>
2017-11-07 17:45:53 -0600

    adding details to open board

--- src/main/java/OpenBoard.java
+++ src/main/java/OpenBoard.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class OpenBoard {
-
+	String boardname;
+	public void boardDisplay()
+	{
+		System.out.println("Do you want to open an existing board?(Input Yes or No)");
+		Scanner inputReader = new Scanner (System.in);
+		String input = inputReader.nextLine();
+		if(input.equals("yes"))
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
 }
