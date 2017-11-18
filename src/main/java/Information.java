6e213c0ff68a29e22708a560b6c3bf96b3c00ff6
nvattipalli <nvattipalli@uh.edu>
2017-11-18 17:57:26 -0600

    added question

--- src/main/java/Information.java
+++ src/main/java/Information.java
@@ -5,6 +5,16 @@ public class Information {
 		help();
 	}
 	public void help(){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("1.) What is trello?");
+		System.out.println("2.) How can I register to Trello?");
+		System.out.println("3.) How can I login into Trello?");
+		System.out.println("4.) How can I reset my password?");
+		System.out.println("5) How can I go to welcome page");
+		System.out.println("Enter a number for the question that you wish to view: ");
+		checkAnswers(scan.nextInt());
+	}
+	public void checkAnswers(int input){
 		
 	}
 }
