6682c4dd64b2f9ef384d7b21a4816b2ea198fd51
nvattipalli <nvattipalli@uh.edu>
2017-11-18 17:48:11 -0600

    added information class

--- src/main/java/Information.java
+++ src/main/java/Information.java
@@ -1,4 +1,10 @@
-
+import java.util.Scanner;
 public class Information {
-
+	public void information() {
+		System.out.println("*****What do you want to know?*****");
+		help();
+	}
+	public void help(){
+		
+	}
 }
