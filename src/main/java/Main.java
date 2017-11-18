2e8a62e1915089db1047e8ff054bf0d7bed7b790
nvattipalli <nvattipalli@uh.edu>
2017-11-18 17:32:27 -0600

    removed the commented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,31 +34,7 @@ public class Main {
  		default:
  			System.out.println("Invalid Option selected!");
 		}
-		/*String x=" ";
-	System.out.println("welcome to trello \n");
-	System.out.println("Already Registered? (Yes/No) or enter reset for password reset");
-	Scanner s = new Scanner(System.in);
-	String input=s.nextLine();
-	if(input.equalsIgnoreCase("yes"))
-	{
-	Login l=new Login();
-	l.login();
-	Board b=new Board();
-	b.board();
-	}
-	else if(input.equalsIgnoreCase("no")){
-		Registration reg=new Registration();
-		reg.Register();
-	}
-	else if (input.equalsIgnoreCase("reset")){
-		Login l1 = new Login();
-		l1.ForgotPassword();
-	}
-	else{
-		x="Invalid";
-	}
-	return x;*/
-		
+			
 	}
 
 }
