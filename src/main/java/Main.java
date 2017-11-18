2aea3d2538f7d8fb7461657e5389ac719c57bd48
nvattipalli <nvattipalli@uh.edu>
2017-11-18 17:31:31 -0600

    modified the welcome class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,38 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args)
 	{
+		System.out.println("welcome to trello \n");
 		welcome();
 		
 	}
 	public static String welcome(){
-		String x=" ";
+		System.out.println("Choose Options \n");
+		System.out.println("Enter 1 to Login\n Enter 2 to Register\n Enter 3 for Password Reset\n Enter 4 for FAQs\n Enter 5 to Exit\n");
+		Scanner scan = new Scanner(System.in);
+		int input=scan.nextInt();
+		switch(input){
+		case 1:
+ 			Login l = new Login();
+ 			l.login();
+ 			break;
+ 		case 2: 
+ 			Registration reg=new Registration();
+ 			reg.Register();
+   			break;
+ 		case 3:
+ 			Login l1 = new Login();
+ 			l1.ForgotPassword();
+ 			break;
+ 		case 4:
+ 			
+ 			break;
+ 		case 5:
+	 		System.out.println("Terminataing the application");
+	 		break;
+ 		default:
+ 			System.out.println("Invalid Option selected!");
+		}
+		/*String x=" ";
 	System.out.println("welcome to trello \n");
 	System.out.println("Already Registered? (Yes/No) or enter reset for password reset");
 	Scanner s = new Scanner(System.in);
@@ -30,7 +57,7 @@ public class Main {
 	else{
 		x="Invalid";
 	}
-	return x;
+	return x;*/
 		
 	}
 
