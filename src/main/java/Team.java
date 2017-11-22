a9deda83c8b398b05e2ac902e5cacf875a213764
nvattipalli <nvattipalli@uh.edu>
2017-11-21 20:15:15 -0600

    made changes to team.java

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -12,15 +12,15 @@ public class Team {
  String teamname;
  String teamdesc;
  String values;
- Scanner inputReader = new Scanner (System.in);
+ Scanner scan = new Scanner (System.in);
  public void getInput()
  {
-  Scanner inputReader = new Scanner (System.in);
+  Scanner scan = new Scanner (System.in);
   System.out.println("Please enter Team Name:");
-   teamname  = inputReader.nextLine();
+   teamname  = scan.nextLine();
   System.out.println("Please enter Team Description:");
-       teamdesc  = inputReader.nextLine();
-       inputReader.close();
+       teamdesc  = scan.nextLine();
+       scan.close();
  }
  
  public void team()
@@ -32,41 +32,57 @@ public class Team {
     Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/Trello","root","root");
    Statement s = null;
   
-  Scanner inputReader = new Scanner (System.in);
-  System.out.println("Press 1 to create team /n Press 2 to create business team ");
-  int option = inputReader.nextInt();
-  if (option == 1)
+  Scanner scan = new Scanner (System.in);
+  System.out.println("Press 1 to create team /n Press 2 to view teams /n Press 3 to add members to team ");
+  int option = scan.nextInt();
+  switch(option)
   {
-   System.out.println("---Create Team---");
-   getInput();
-    values = "INSERT INTO team (team_name,team_desc) " + "VALUES ('" +teamname+ "', '" +teamdesc+"')";
-   
+  case 1:
+	  createTeam();
+	  break;
+  }
+  }
+  catch(Exception e){ 
+	  System.out.println(e);
+	  }
+ }
+  public void createTeam()
+  {
+	  try
+	  {
+		  System.out.println("Press 1 to create Team /n Press 2 to create Business Team");
+		  int input = scan.nextInt();
+		  if (input == 1)
+		  {
+		   System.out.println("---Create Team---");
+		   getInput();
+		    values = "INSERT INTO team (team_name,team_desc) " + "VALUES ('" +teamname+ "', '" +teamdesc+"')";
+		   
+		  }
+		  
+		  else if (input == 2) {
+		   System.out.println("---Create Business Team---");
+		   getInput();
+		   
+		    values = "INSERT INTO business_team (bus_team_name,bus_team_desc) " + "VALUES ('" +teamname+ "', '" +teamdesc+"')";
+		  }
+	  }
+	  catch(Exception e){ 
+		  System.out.println(e);
+		  }
   }
   
-  else if (option == 2) {
-   System.out.println("---Create Business Team---");
-   getInput();
-   
-    values = "INSERT INTO business_team (bus_team_name,bus_team_desc) " + "VALUES ('" +teamname+ "', '" +teamdesc+"')";   
-  }
-  s = conn.createStatement();
-  s.executeUpdate(values); 
-  System.out.println(teamname + " Team created Successfully");
-  inputReader.close();
-  }
-  catch(Exception e){ System.out.println(e);}
- }
  public void addMembersToTeam()
  {  
   ArrayList<String> members = new ArrayList<String>(); 
    System.out.println("Press/n 1.to add members to Team /n  Press 2. to add members to Business Team ");
-   int input = inputReader.nextInt();
-   inputReader.nextLine(); 
+   int input = scan.nextInt();
+   scan.nextLine(); 
    System.out.println("Enter Team Name:");
-   String teamName  = inputReader.nextLine();
+   String teamName  = scan.nextLine();
    String membername;
    System.out.println("Enter Member Name:");
-   membername  = inputReader.nextLine();
+   membername  = scan.nextLine();
          members.add(membername); 
          addToDatabase(membername,teamName,input);
     System.out.println("Members added to the team successfully!");
