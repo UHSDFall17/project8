df839cab9fe7023be52ce691890393a7e775bbd5
nvattipalli <nvattipalli@uh.edu>
2017-11-30 15:33:45 -0600

    added more functionality to test cases

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 public class TestCase1 {
 
 	@Test
-	public void LoginSuccess() {
+	public void WelcomePage() {
 		Login log=new Login();
 		String result=log.login();
+		System.out.println(result);
 		assertEquals("Login Successfull!",result);
 		
 	}
--- src/test/java/TestCase4.java
+++ src/test/java/TestCase4.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestCase4 {
 
 	@Test
-	public void Registration_Successfull() {
+	public void RegistrationFlow() {
 		Registration reg=new Registration();
 		String result=reg.Register();
 		assertEquals("Registration Successfull!!",result);
